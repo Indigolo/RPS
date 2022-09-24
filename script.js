483507e73a24a2e1cfd4ed27da1ff9f3dc59166b
indigolo <jijavadzeachiko@gmail.com>
2022-09-24 17:39:32 +0200

    Add function, that plays single round

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 function getComputerChoice(){
+    
     let rng = Math.floor((Math.random() * 3)) + 1;
 
-    if(rng == 1){
-        let choice = "Rock";
-    }
-
-    else if(rng == 2){
-        let choice = "Paper";
-    }
-
-    else{
-        let choice = "Scissors";
-    }
+    if(rng == 1)
+    return choice = "Rock";
     
+
+    else if(rng == 2)
+    return choice = "Paper";
+    
+
+    else
+    return choice = "Scissors";
+    
+    
+}
+
+function round(playerSelection, computerSelection){
+
+    if(playerSelection == computerSelection)
+    alert("Draw");
+
+    else if(playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "SCISSORS")
+    alert("Victory");
+
+    else if(playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "ROCK")
+    alert("Victory");
+
+    else if(playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "PAPER")
+    alert("Victory");
+
+    else
+    alert("Defeat");
 }
